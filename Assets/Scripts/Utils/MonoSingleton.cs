e05e51474f0de3096c5fc9cbfa417170c9681844
Bernhard Raml <B.Raml@gmx.at>
2015-05-14 15:04:28 +0100

    added monosingleton

---
+++ Assets/Scripts/Utils/MonoSingleton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+	private static T instance = null;
+	
+	public static T Instance 
+	{
+		get 
+		{
+			instance = instance ?? FindObjectOfType<T>();
+			if (instance == null)
+			{
+				Debug.LogError(string.Format("Couldn't find {0}. Please add it to the scene.", typeof(T).Name));
+			}
+
+			return instance;
+		}
+	}
+}
