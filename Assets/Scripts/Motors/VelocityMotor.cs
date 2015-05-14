54de42253b07f402a5a07ed4ffa750bfc5097e64
SwamyDev <B.Raml@gmx.at>
2015-05-14 18:05:36 +0100

    fixed velocity motor inherited member warning

--- Assets/Scripts/Motors/VelocityMotor.cs
+++ Assets/Scripts/Motors/VelocityMotor.cs
@@ -8,35 +8,35 @@ public class VelocityMotor : MonoBehaviour, IMoveable
     [SerializeField]
     private float speed;
 
-    private Rigidbody rigidbody3D;
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody body3D;
+    private Rigidbody2D body2D;
 
     private Vector3 direction;
     private float inputMagnitude;
 
     void Awake()
     {
-        rigidbody3D = GetComponent<Rigidbody>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        body3D = GetComponent<Rigidbody>();
+        body2D = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        if (rigidbody2D != null)
+        if (body2D != null)
             Update2D();
 
-        if (rigidbody3D != null)
+        if (body3D != null)
             Update3D();
     }
 
     private void Update3D()
     {
-        rigidbody3D.velocity = direction * speed * inputMagnitude;
+        body3D.velocity = direction * speed * inputMagnitude;
     }
 
     private void Update2D()
     {
-        rigidbody2D.velocity = direction * speed * inputMagnitude;
+        body2D.velocity = direction * speed * inputMagnitude;
     }
 
     public void Move(Vector3 input)
