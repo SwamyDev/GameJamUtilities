5dba19ddfe3dad2064616ca9b2c64ce6e41cce23
SwamyDev <B.Raml@gmx.at>
2015-05-14 18:09:07 +0100

    added bare paltformer motor

---
+++ Assets/Scripts/Motors/PlatformerMotor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JamUtilities {
+
+public class PlatformerMotor : MonoBehaviour, IMoveable
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float jumpHeight;
+    [SerializeField]
+    private float jumpSpeed;
+
+    private Rigidbody2D body;
+    private float inputMagnitude;
+    private bool isRequestingJump;
+    private bool isLanded;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 velocity = Vector2.zero;
+        if (isRequestingJump && isLanded)
+        {
+            isLanded = false;
+            velocity.y = jumpSpeed;
+        }
+
+        velocity.x = inputMagnitude * speed;
+        body.velocity = velocity;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        isLanded = true;
+    }
+
+    public void Move(Vector3 input)
+    {
+        isRequestingJump = !Mathf.Approximately(input.y, 0.0f);
+        inputMagnitude = input.x;
+    }
+}
+
+}   // namespace JamUtilities
